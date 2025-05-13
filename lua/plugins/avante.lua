75891b5011a811a6a620e5c33f71ac32c6997b0e
SCUTBrothers <47417892+SCUTBrothers@users.noreply.github.com>
2025-05-13 08:18:26 +0800

    移除vscode相关的配置和keymap

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -2,7 +2,6 @@ return {
   "yetone/avante.nvim",
   event = "VeryLazy",
   version = false, -- Never set this value to "*"! Never!
-  vscode = false,
   opts = {
     -- add any opts here
     -- for example
