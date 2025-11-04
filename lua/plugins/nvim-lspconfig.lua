be54cda052f15231ce0f2fc5a71a56aeadaf3665
SCUTBrothers <1260812658@qq.com>
2025-11-04 09:42:50 +0800

    change lsp keymaps

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,12 +1,5 @@
 return {
   "neovim/nvim-lspconfig",
-  init = function()
-    local keys = require("lazyvim.plugins.lsp.keymaps").get()
-    -- disable a keymap
-    keys[#keys + 1] = { "K", false }
-    -- add a keymap
-    keys[#keys + 1] = { "gh", vim.lsp.buf.hover }
-  end,
   opts = {
     inlay_hints = { enabled = false },
     diagnostics = {
@@ -14,6 +7,14 @@ return {
     },
     document_highlight = {
       enabled = false,
+    },
+    servers = {
+      ["*"] = {
+        keys = {
+          { "K", "", enabled = false },
+          { "gh", vim.lsp.buf.hover },
+        },
+      },
     },
   },
 }
