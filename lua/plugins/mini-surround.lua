32e3df408f75ae6122e206056a5a419c22430de7
chenb11 <chenb11@xiaopeng.com>
2026-02-20 22:14:34 +0800

    feat: obsidian 图片粘贴/gf 跳转，mini.surround markdown 快捷包围
    - obsidian: leader-mp 粘贴剪贴板图片到当前文件目录 _images/，自动命名
    - obsidian: gf 在图片链接上用 :edit 打开（snacks image 渲染），其他走默认
    - mini.surround: 添加 B(加粗) I(斜体) C(代码) K(代码块) S(删除线) H(高亮)

---
+++ lua/plugins/mini-surround.lua
@@ -0,0 +1,19 @@
+return {
+  "echasnovski/mini.surround",
+  opts = {
+    custom_surroundings = {
+      -- B = markdown 加粗 **text**
+      B = { output = { left = "**", right = "**" } },
+      -- I = markdown 斜体 *text*
+      I = { output = { left = "*", right = "*" } },
+      -- C = markdown 行内代码 `text`
+      C = { output = { left = "`", right = "`" } },
+      -- K = markdown 代码块
+      K = { output = { left = "```\n", right = "\n```" } },
+      -- S = markdown 删除线 ~~text~~
+      S = { output = { left = "~~", right = "~~" } },
+      -- H = markdown 高亮 ==text==
+      H = { output = { left = "==", right = "==" } },
+    },
+  },
+}
--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -3,6 +3,32 @@ return {
   version = "*", -- recommended, use latest release instead of latest commit
   lazy = true,
   ft = "markdown",
+  keys = {
+    {
+      "<leader>mp",
+      function()
+        -- 将图片保存到当前文件所在目录的 _images 下
+        local buf_dir = vim.fn.expand("%:p:h")
+        local img_dir = buf_dir .. "/_images"
+        vim.fn.mkdir(img_dir, "p")
+        local timestamp = os.date("%Y%m%d%H%M%S")
+        local fname = "image-" .. timestamp .. ".png"
+        local filepath = img_dir .. "/" .. fname
+        -- 使用 pngpaste 保存剪贴板图片
+        local result = vim.fn.system({ "pngpaste", filepath })
+        if vim.v.shell_error ~= 0 then
+          vim.notify("粘贴图片失败: " .. result, vim.log.levels.ERROR)
+          return
+        end
+        -- 插入相对路径的 markdown 图片链接
+        local link = string.format("![%s](%s)", fname, "_images/" .. fname)
+        vim.api.nvim_put({ link }, "c", true, true)
+        vim.notify("已粘贴图片: " .. fname, vim.log.levels.INFO)
+      end,
+      desc = "Paste image",
+      ft = "markdown",
+    },
+  },
   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
   -- event = {
   --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
@@ -29,6 +55,31 @@ return {
       },
     },
 
+    -- gf 映射：图片文件在 buffer 中打开（snacks image 渲染），其他走 obsidian 默认逻辑
+    mappings = {
+      ["gf"] = {
+        action = function()
+          local util = require("obsidian.util")
+          if util.cursor_on_markdown_link() then
+            local line = vim.api.nvim_get_current_line()
+            local path = line:match("%[.-%]%((.-)%)")
+            if path and (path:match("%.png$") or path:match("%.jpe?g$") or path:match("%.gif$") or path:match("%.webp$") or path:match("%.svg$")) then
+              local buf_dir = vim.fn.expand("%:p:h")
+              local abs_path = buf_dir .. "/" .. path
+              if vim.fn.filereadable(abs_path) == 1 then
+                vim.cmd("edit " .. vim.fn.fnameescape(abs_path))
+              else
+                vim.notify("图片文件不存在: " .. abs_path, vim.log.levels.ERROR)
+              end
+              return
+            end
+          end
+          return vim.cmd("ObsidianFollowLink")
+        end,
+        opts = { noremap = false, expr = false, buffer = true, desc = "Go to file" },
+      },
+    },
+
     -- 禁用 Obsidian 的 UI，使用 render-markdown.nvim 代替
     ui = {
       enable = false,
@@ -36,6 +87,20 @@ return {
 
     -- 不自动插入 yaml  frontmatter
     disable_frontmatter = true,
+
+    -- 图片粘贴配置
+    attachments = {
+      -- 图片保存到当前文件同级的 _images 目录
+      img_folder = "_images",
+      ---@param client obsidian.Client
+      ---@param path obsidian.Path
+      ---@return string
+      img_text_func = function(client, path)
+        -- 生成相对路径的 markdown 图片链接
+        path = client:vault_relative_path(path) or path
+        return string.format("![%s](%s)", path.name, path)
+      end,
+    },
     -- see below for full list of options 👇
   },
 }
